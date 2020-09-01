1be7cddad3242bdb6a573cd1db268b432f3f15b0
Guillermo Angeris <angeris@stanford.edu>
2020-09-01 11:06:42 -0700

    silly mistakes

--- src/agent.jl
+++ src/agent.jl
@@ -60,18 +60,17 @@ function find_projection!(a::AgentModel)
     for (name, position) in a.object_position
         D, U = a.object_uncertainty[name]
 
-        λ = @variable(model)
-
+        λ = @variable(model, lower_bound=0.0)
         t = quad_over_lin(model, U'*(x + λ * position), 1 ./ D .+ λ)
         @constraint(model, sum(a.current_point.^2) - 2*(a.current_point' * x) + sum(t)
-        <= (position' * (D .* position)) * λ - λ)
+        <= (position' * (U * (D .* (U' * position)))) * λ - λ)
         
-        @constraint(model, λ >= 0)
     end
 
     optimize!(model)
 
     a.solved = true
+    a.projected_point = value.(x)
 
-    return a.projected_point = value.(x)
+    return a.projected_point
 end
